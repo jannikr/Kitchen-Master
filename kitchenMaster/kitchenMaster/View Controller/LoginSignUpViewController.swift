a7584862d2edcfa62a696d3f41cd5623deea08de
Jan Rückert <janruckert@Jans-MBP.fritz.box>
2019-12-08 13:49:02 +0100

    LoginSignUp View Controller

---
+++ kitchenMaster/kitchenMaster/View Controller/LoginSignUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginSignUpViewController.swift
+//  kitchenMaster
+//
+//  Created by Jan Rückert on 08.12.19.
+//  Copyright © 2019 Jan Rückert. All rights reserved.
+//
+
+import UIKit
+
+class LoginSignUpViewController: UIViewController {
+    
+    //UI view properties
+    @IBOutlet weak var anmeldenButton: UIButton!
+    @IBOutlet weak var registrierenButton: UIButton!
+    
+    
+    //First loading function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpProperties()
+        
+    }
+    
+    //Sets up the UI Elements
+    func setUpProperties(){
+        
+        //Anmelden Button
+        anmeldenButton.layer.cornerRadius = anmeldenButton.layer.frame.height/2
+        
+    }
+
+}
