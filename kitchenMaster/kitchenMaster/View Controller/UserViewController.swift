c6455c029c81a0ee3408534f1479b9e8dfbba709
Jan Rückert <janruckert@Jans-MBP.fritz.box>
2019-12-16 23:19:28 +0100

    icons + viewcontroller

---
+++ kitchenMaster/kitchenMaster/View Controller/UserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserViewController.swift
+//  kitchenMaster
+//
+//  Created by Jan Rückert on 16.12.19.
+//  Copyright © 2019 Jan Rückert. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
