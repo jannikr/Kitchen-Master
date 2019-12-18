26a6562f65c853687271b67fffc6bae2eb702952
Jan Rückert <janruckert@Jans-MacBook-Pro.local>
2019-12-18 12:33:35 +0100

    rezept cells

---
+++ kitchenMaster/Cells/RezeptCell.swift
@@ -0,0 +1,24 @@
+//
+//  RezeptCell.swift
+//  kitchenMaster
+//
+//  Created by Jan Rückert on 18.12.19.
+//  Copyright © 2019 Jan Rückert. All rights reserved.
+//
+
+import UIKit
+
+class RezeptCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
